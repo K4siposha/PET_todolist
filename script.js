86957a31927f86372ba32151b23468e382f1066c
Дмитрий Беспалов <dmitrij.bespalov.98@mail.ru>
2025-03-19 19:40:45 +0300

    1

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 "use strict";
-let addTask = () => {
-    f
-}
+// обязательно использовать localstorage
+// чтобы при перезагрузке страницы сохранялось все
+// из функционала добавление, удаление, редактирование задач, возможность помечать выполненные, фильтрация
+
+const list = document.querySelector('#taskList');
+const input = document.querySelector('#taskInput');
+const btn = document.querySelector('#addTaskBtn');
+
+btn.addEventListener('li', (e) => {
+    e.preventDefault();
+    const taskText = input.value;
+    const taskHTML = `
+                      <li id="list">
+                        <span id="task-title">${taskText}</span>
+                      </li>`;
+    list.insertAdjacentElement('beforeend', taskHTML);
+});
