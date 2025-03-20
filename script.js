e4e2f8fac66ac5c7051874df2483d054cc3e266f
Дмитрий Беспалов <dmitrij.bespalov.98@mail.ru>
2025-03-20 17:47:47 +0300

    1

--- script.js
+++ script.js
@@ -3,16 +3,24 @@
 // чтобы при перезагрузке страницы сохранялось все
 // из функционала добавление, удаление, редактирование задач, возможность помечать выполненные, фильтрация
 
-const list = document.querySelector('#taskList');
-const input = document.querySelector('#taskInput');
-const btn = document.querySelector('#addTaskBtn');
+function add() {
+    const taskText = document.getElementById('taskInput').value;
+    if (taskText.trim() === '') return;
 
-btn.addEventListener('li', (e) => {
-    e.preventDefault();
-    const taskText = input.value;
-    const taskHTML = `
-                      <li id="list">
-                        <span id="task-title">${taskText}</span>
-                      </li>`;
-    list.insertAdjacentElement('beforeend', taskHTML);
-});
+    let taskId = Date.now().toString();
+
+    const taskElement = document.createElement('li');
+    taskElement.id = taskId;
+
+    const text = document.createElement('span');
+    text.dataset.taskId = taskId;
+    text.textContent = taskText;
+
+    taskElement.appendChild(text);
+
+    document.getElementById('taskList').appendChild(taskElement);
+    document.getElementById('taskInput').value = '';
+
+}
+
+document.getElementById('addTaskBtn').addEventListener('click', add);
