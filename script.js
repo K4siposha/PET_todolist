dd591ce6c47d7a3e4438626deb9e7ceb2344bf9a
Дмитрий Беспалов <dmitrij.bespalov.98@mail.ru>
2025-03-23 21:15:42 +0300

    1

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 "use strict";
 // обязательно использовать localstorage
-// чтобы при перезагрузке страницы сохранялось все
 // из функционала добавление, удаление, редактирование задач, возможность помечать выполненные, фильтрация
 
 function add() {
@@ -12,15 +11,45 @@ function add() {
     const taskElement = document.createElement('li');
     taskElement.id = taskId;
 
-    const text = document.createElement('span');
+    let text = document.createElement('span');
     text.dataset.taskId = taskId;
     text.textContent = taskText;
 
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = 'Delete';
+    deleteButton.dataset.taskId = taskId;
+
+    const editButton = document.createElement("button");
+    editButton.textContent = 'Edit';
+    editButton.dataset.taskId = taskId;
+
     taskElement.appendChild(text);
+    taskElement.appendChild(editButton);
+    taskElement.appendChild(deleteButton);
 
     document.getElementById('taskList').appendChild(taskElement);
     document.getElementById('taskInput').value = '';
 
+    setupEventListeners(deleteButton, text, taskElement, editButton);
+}
+
+function setupEventListeners(deleteButton, text, taskElement, editButton) {
+    deleteButton.addEventListener('click', function() {
+        const taskIdToDelete = this.dataset.taskId;
+        const taskToDelete = document.getElementById(taskIdToDelete);
+        if (taskToDelete) {
+            taskToDelete.remove();
+        }
+    });
+    editButton.addEventListener('click', function() {
+        const taskElement = this.parentNode;
+        let text = taskElement.querySelector('span');
+        let newText = prompt('Название', text.textContent);
+
+        if (newText !== null) {
+            text.textContent = newText;
+        }
+    });
 }
 
 document.getElementById('addTaskBtn').addEventListener('click', add);
